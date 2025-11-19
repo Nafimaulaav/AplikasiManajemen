6316ffb8ddc6e909af35b520f2b224e91ad3f624
raflinurramdhan21-dev <raflinurramdhan21@gmail.com>
2025-11-19 21:22:38 +0700

    tabel tanaman

--- AplikasiManajemenPTKarsadia/database/migrations/2025_11_18_132811_create_tanaman_table.php
+++ AplikasiManajemenPTKarsadia/database/migrations/2025_11_18_132811_create_tanaman_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tanaman', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('id_tanaman', 10)->primary();
+            $table->string('varietas', 100);
+            $table->date('tanggal_tanam');
+            $table->integer('jumlah_tanaman');
+            $table->enum('status_tumbuh', ['tumbuh', 'panen', 'mati']);
+            $table->date('perkiraan_panen');
+
+            // foreign key id greenhouse
+            $table->string('id_greenhouse', 10);
+            $table->foreign('id_greenhouse')->references('id_greenhouse')->on('greenhouse')->onDelete('cascade');
         });
     }
 
