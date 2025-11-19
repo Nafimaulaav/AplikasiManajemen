4fb1cc1471cdaafca266f36577e27c82c249e437
raflinurramdhan21-dev <raflinurramdhan21@gmail.com>
2025-11-19 22:46:31 +0700

    coba bikin model greenhouse

--- AplikasiManajemenPTKarsadia/app/Models/CobaGreenhouse.php
+++ AplikasiManajemenPTKarsadia/app/Models/CobaGreenhouse.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class CobaUser extends Authenticatable
+class CobaGreenhouse extends Authenticatable
 {
     // buat nama tabel
-    protected $table = 'users';
+    protected $table = 'greenhouse';
     // buat bikin primary key
-    protected $primaryKey = 'id_user';
+    protected $primaryKey = 'id_greenhouse';
     // biar kaga auto increment, soalnya pake string, pengen biar U0001 gitu dah
     public $incrementing = false;
     // buat tipe data primary key string
@@ -22,16 +22,12 @@ class CobaUser extends Authenticatable
 
     // kolom yg bisa diisi
     protected $fillable = [
-        'id_user',
-        'nama_user',
-        'email',
-        'password',
-        'role'
-    ];
-
-    // biar password ga keliatan
-    protected $hidden = [
-        'password',
+        'id_greenhouse',
+        'nama_greenhouse',
+        'lokasi_greenhouse',
+        'status_greenhouse',
+        'tanggal_operasional',
+        'kapasitas'
     ];
 }
 
