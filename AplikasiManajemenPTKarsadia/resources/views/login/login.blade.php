c11dc87fe797b7772af333ffd7bb99944d8c3a09
Yanuar <yanyanwhy12@gmail.com>
2025-12-11 15:25:28 +0700

    buat header

---
+++ AplikasiManajemenPTKarsadia/resources/views/login/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+
+
+</head>
+<body>
+
+    <div class="login-container">
+        <div class="login-card">
+            @yield('content')
+        </div>
+    </div>
+
+</body>
+</html>
