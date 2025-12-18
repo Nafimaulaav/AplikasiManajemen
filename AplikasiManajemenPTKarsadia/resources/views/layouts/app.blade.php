ec97bc402a6a6b211732ba67a705569e673115aa
Your Name <nafymaula7@gmail.com>
2025-12-18 15:46:05 +0700

    header sidebar content

---
+++ AplikasiManajemenPTKarsadia/resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <title>Karsadia Management</title>
+ </head>
+ <body>
+    @include('partials.header')
+    @include('partials.sidebar')
+    <div class="content">
+        @yield('content')
+    </div>
+ </body>
+ </html>
